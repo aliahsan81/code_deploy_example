f191979f1b2689fdd202694b898606b919fd5030
aliahsan81 <ali.ahsan@zameen.com>
2016-12-13 21:45:05 +0500

    Update restartapache.sh

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.orginal.bk > /var/log/restartapache.out 2>&1
+mkdir /mnt/data/vhost/wwww/
 service httpd restart > /var/log/restartapache.out 2>&1
